65406a97f598e10d530c9c6e470a45c3fd3c5893
Zipo Namberwan <dopokan@gmail.com>
2023-10-23 15:52:06 +0700

    first commit

---
+++ database/migrations/2023_10_23_082509_init_database.php
@@ -0,0 +1,84 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subject', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+        });
+
+        Schema::create('characteristic', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+        });
+
+        Schema::create('characteristic_value', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->foreignId('characteristic_id')->constrained('characteristic');
+        });
+
+        Schema::create('row', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+        });
+
+        Schema::create('row_value', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->foreignId('row_id')->constrained('row');
+        });
+
+        Schema::create('period', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+        });
+
+        Schema::create('period_value', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->foreignId('period_id')->constrained('period');
+        });
+
+        Schema::create('unit', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+        });
+
+        Schema::create('indicator', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->foreignId('subject_id')->constrained('subject');
+            $table->foreignId('characteristic_id')->nullable()->constrained('characteristic');
+            $table->foreignId('period_id')->constrained('period');
+            $table->foreignId('row_id')->constrained('row');
+            $table->foreignId('unit_id')->nullable()->constrained('unit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
